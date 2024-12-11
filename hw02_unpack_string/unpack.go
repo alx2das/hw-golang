12f70c02c1573473e2f1b8a5e642c26c4882d07e
Alex Buylov <a.buylov@pnlfin.tech>
2024-12-12 00:23:45 +0200

    update

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -4,29 +4,56 @@ import (
 	"errors"
 	"strconv"
 	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
 	var result strings.Builder
-	var prev string
+	var isEscaped bool
 
-	for _, str := range input {
-		if count, err := strconv.Atoi(string(str)); err == nil {
-			if prev == "" {
-				return "", ErrInvalidString
+	for i, item := range input {
+		if unicode.IsDigit(item) && i == 0 {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsDigit(item) && unicode.IsDigit(rune(input[i-1])) && input[i-2] != '\\' {
+			return "", ErrInvalidString
+		}
+
+		if unicode.IsLetter(item) && isEscaped {
+			return "", ErrInvalidString
+		}
+
+		if item == '\\' && !isEscaped {
+			isEscaped = true
+			continue
+		}
+
+		if isEscaped {
+			result.WriteRune(item)
+			isEscaped = false
+			continue
+		}
+
+		if unicode.IsDigit(item) {
+			count, _ := strconv.Atoi(string(item))
+			if count == 0 {
+
+				var str = result.String()
+				result.Reset()
+				result.WriteString(str[:len(str)-1])
+
+				continue
 			}
 
-			result.WriteString(strings.Repeat(prev, count))
-			prev = ""
-		} else {
-			result.WriteString(prev)
-			prev = string(str)
+			result.WriteString(strings.Repeat(string(input[i-1]), count-1))
+			continue
 		}
-	}
 
-	result.WriteString(prev)
+		result.WriteRune(item)
+	}
 
 	return result.String(), nil
 }
