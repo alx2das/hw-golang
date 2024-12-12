36223a1ffcf87e74d34d2322fc0c293af78b2cb7
Alex Buylov <a.buylov@pnlfin.tech>
2024-12-12 12:31:38 +0200

    add comments

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,43 +10,56 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
+	// пустая строка - коррект
+	if input == "" {
+		return "", nil
+	}
+
+	// начало с цифры - ошибка
+	if unicode.IsDigit(rune(input[0])) {
+		return "", ErrInvalidString
+	}
+
 	var result strings.Builder
 	var isEscaped bool
 
 	for i, item := range input {
-		if unicode.IsDigit(item) && i == 0 {
-			return "", ErrInvalidString
-		}
-
+		// если пытаемся повторить неэкранированную цифру
 		if unicode.IsDigit(item) && unicode.IsDigit(rune(input[i-1])) && input[i-2] != '\\' {
 			return "", ErrInvalidString
 		}
 
+		// если пытаемся повторить букву
 		if unicode.IsLetter(item) && isEscaped {
 			return "", ErrInvalidString
 		}
 
+		// начало экранирование
 		if item == '\\' && !isEscaped {
 			isEscaped = true
 			continue
 		}
 
+		// записываем экранированный символ
 		if isEscaped {
 			result.WriteRune(item)
 			isEscaped = false
 			continue
 		}
 
+		// повторим предыдущий символ `count` раз
 		if unicode.IsDigit(item) {
 			count, _ := strconv.Atoi(string(item))
+
+			// удалим предыдущий символ
 			if count == 0 {
 				str := result.String()
 				result.Reset()
 				result.WriteString(str[:len(str)-1])
-
 				continue
 			}
 
+			// дублируем предыдущий символ `count` раз
 			result.WriteString(strings.Repeat(string(input[i-1]), count-1))
 			continue
 		}
