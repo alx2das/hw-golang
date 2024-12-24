21f16b5ce11bcbddb62e74ae88c266aa76dbd805
Alex Buylov <a.buylov@pnlfin.tech>
2024-12-24 15:01:41 +0200

    added implementation of methods

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -22,41 +22,46 @@ type list struct {
 	size int
 }
 
-func (l list) Len() int {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) Front() *ListItem {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) Back() *ListItem {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) PushFront(v interface{}) *ListItem {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) PushBack(v interface{}) *ListItem {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) Remove(i *ListItem) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (l list) MoveToFront(i *ListItem) {
-	//TODO implement me
-	panic("implement me")
-}
-
 func NewList() List {
 	return new(list)
+}
+
+func (l *list) Len() int {
+	return l.size
+}
+
+func (l *list) Front() *ListItem {
+	if l.size == 0 {
+		return nil
+	}
+
+	return l.head
+}
+
+func (l *list) Back() *ListItem {
+	if l.size == 0 {
+		return nil
+	}
+
+	return l.tail
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *list) Remove(i *ListItem) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	//TODO implement me
+	panic("implement me")
 }
