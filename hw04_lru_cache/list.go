2092bbf32f72ece4737541b6424d521413dab8b5
Alex Buylov <a.buylov@pnlfin.tech>
2024-12-24 14:58:28 +0200

    created method templates

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,44 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	head *ListItem
+	tail *ListItem
+	size int
+}
+
+func (l list) Len() int {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) Front() *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) Back() *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) PushFront(v interface{}) *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) PushBack(v interface{}) *ListItem {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) Remove(i *ListItem) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (l list) MoveToFront(i *ListItem) {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewList() List {
