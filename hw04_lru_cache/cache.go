a4c877d703d9fa531212b91fa064ab1874a0c7be
Alex Buylov <a.buylov@pnlfin.tech>
2024-12-27 16:31:30 +0200

    fix cache & add tests

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04lrucache
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -11,7 +13,8 @@ type Cache interface {
 type lruCache struct {
 	capacity int
 	queue    List
-	items    map[Key]*ListItem
+	items    sync.Map
+	mu       sync.Mutex
 }
 
 type cacheItem struct {
@@ -21,34 +24,55 @@ type cacheItem struct {
 
 func (c *lruCache) Set(key Key, value interface{}) bool {
 	// если элемент присутствует в словаре
-	if node, exists := c.items[key]; exists {
+	if existingNode, exist := c.items.Load(key); exist {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+
+		node := existingNode.(*ListItem)
 		node.Value.(*cacheItem).value = value
+
+		// обновить значение и переместить элемент в начало очереди
+		c.items.Store(key, node)
 		c.queue.MoveToFront(node)
+
+		// успешно обновлен
 		return true
 	}
 
-	// если размер очереди больше ёмкости кэша
-	if len(c.items) >= c.capacity {
-		// удаляем последний элемент
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	// добавить в словарь и в начало очереди
+	if c.queue.Len() >= c.capacity {
 		tailNode := c.queue.Back()
 		if tailNode != nil {
 			tailItem := tailNode.Value.(*cacheItem)
-			delete(c.items, tailItem.key) // удаляем из словаря
-			c.queue.Remove(tailNode)      // удаляем из списка
+
+			c.items.Delete(tailItem.key)
+			c.queue.Remove(tailNode)
 		}
 	}
 
-	// добавить в словарь и в начало очереди
+	// добавляем элемент
 	newItem := &cacheItem{key, value}
 	newNode := c.queue.PushFront(newItem)
-	c.items[key] = newNode
 
+	c.items.Store(key, newNode)
+
+	// успешно добавлен
 	return false
 }
 
 func (c *lruCache) Get(key Key) (interface{}, bool) {
-	if node, exists := c.items[key]; exists {
+	// если элемент присутствует в словаре
+	if existingNode, exist := c.items.Load(key); exist {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+
+		node := existingNode.(*ListItem)
+		// переместить элемент в начало очереди
 		c.queue.MoveToFront(node)
+
 		return node.Value.(*cacheItem).value, true
 	}
 
@@ -56,14 +80,17 @@ func (c *lruCache) Get(key Key) (interface{}, bool) {
 }
 
 func (c *lruCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	c.queue.Clear()
-	c.items = make(map[Key]*ListItem)
+	c.items.Clear()
 }
 
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		items:    sync.Map{},
 	}
 }
